b5b72bc97a921c27141a29fe5ec4b843673476e4
Jan Peterhaensel <jan.peterhaensel@adconova.com>
2023-07-10 11:05:28 +0200

    dskds

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./home.nix
-      <home-manager/nixos>
     ];
 
   # Bootloader.
--- home.nix
+++ home.nix
@@ -1,24 +1,17 @@
 { config, pkgs, ... }:
 
+
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      <home-manager/nixos>
+    ];
   programs.home-manager.enable = true;
-
-  home.username = "psycho";
-  home.homeDirectory = "/home/psycho";
   
-  home.file.".config/qtile".source = ./home-manager/qtile;
+  home-manager.users."psycho" = {
+      home = "/home/psycho"
+    }
 
 
-  programs.zsh = {
-    enable = true;
-  };
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.11";
 }
