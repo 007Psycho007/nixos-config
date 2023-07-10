c6eb9ec4250b15a2fd89e358d5e5d15925a81d8e
Jan Peterhaensel <jan.peterhaensel@adconova.com>
2023-07-10 10:51:02 +0200

    fderror: syntax error, unexpected '=', expecting ';'

--- home.nix
+++ home.nix
@@ -8,7 +8,7 @@
 
   programs.zsh = {
     enable = true;
-  }
+  };
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
