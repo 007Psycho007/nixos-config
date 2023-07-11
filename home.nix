4c8bf126acb908793918d5b916e40b027b334c13
Jan Peterhaensel <jan.peterhaensel@adconova.com>
2023-07-11 10:53:56 +0200

    fdf

--- home.nix
+++ home.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
+    url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
+  }) {
+    doomPrivateDir = ./doom.d;  # Directory containing your config.el, init.el
+                                # and packages.el files
+  };
+in {
+  home-manager.users.psycho.home.packages = [ doom-emacs ];
+}
 
 {
   imports =
