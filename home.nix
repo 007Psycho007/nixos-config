6e7689485a66fd1612fa1ba4a8d61becef73ff5a
Jan Peterhaensel <jan.peterhaensel@adconova.com>
2023-07-11 10:58:10 +0200

    dff

--- home.nix
+++ home.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-    url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
-  }) {
-    doomPrivateDir = ./doom.d;  # Directory containing your config.el, init.el
-                                # and packages.el files
-  };
-in {
-  home-manager.users.psycho.home.packages = [ doom-emacs ];
-}
-
 {
   imports =
     [
@@ -222,6 +211,15 @@ in {
         }
       ];
     };
+    
+    home.packages = [
+      pkgs.callPackage (builtins.fetchTarball {
+      url = https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz;
+        }) {
+      doomPrivateDir = ./doom.d;  # Directory containing your config.el, init.el
+                                # and packages.el files
+  };
+    ]
 
     home.stateVersion = "21.11";
   };
