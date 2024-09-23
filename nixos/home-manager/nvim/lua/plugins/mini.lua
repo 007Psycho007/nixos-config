95618503e95911813770fc5ae3a9fda83dce0ea6
Jan <jan.peterhaensel@adconova.com>
2024-09-23 15:15:45 +0200

    added markdown

--- nixos/home-manager/nvim/lua/plugins/mini.lua
+++ nixos/home-manager/nvim/lua/plugins/mini.lua
@@ -1,5 +1,21 @@
 return {
     'echasnovski/mini.nvim',
     main = "mini.comment",
-    opts = {}
+    opts = {
+        bullet = {
+            -- Turn on / off list bullet rendering
+            enabled = true,
+            -- Replaces '-'|'+'|'*' of 'list_item'
+            -- How deeply nested the list is determines the 'level'
+            -- The 'level' is used to index into the array using a cycle
+            -- If the item is a 'checkbox' a conceal is used to hide the bullet instead
+            icons = { '● ', '○ ', '◆ ', '◇ ' },
+            -- Padding to add to the left of bullet point
+            left_pad = 0,
+            -- Padding to add to the right of bullet point
+            right_pad = 0,
+            -- Highlight for the bullet icon
+            highlight = 'RenderMarkdownBullet',
+    },
+    }
 }
