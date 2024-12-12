67f69a9d48e15cb8e8cc6cd9c5067da5cba69d56
Jan <jan.peterhaensel@adconova.com>
2024-12-12 19:10:39 +0100

    fdfj

--- nixos/home-manager/scripts/lock.sh
+++ nixos/home-manager/scripts/lock.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-i3lock-color -k \
-    --indicator \
-    --inside-color 282C34FF \
-    --ring-color 61AFEF \
-    --insidever-color 61AFEF \
-    --ringver-color 61AFEF \
-    --insidewrong-color E86681 \
-    --ringwrong-color E86671 \
-    --keyhl-color 98C379 \
-    --bshl-color E86671 \
-    -c 282C34FF \
-    --time-color 61AFEF \
-    --date-color 61AFEF
-
+# Check if XDG_SESSION_TYPE is set to "wayland"
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    # Execute hyprlock
+    hyprlock
+else
+    i3lock-color -k \
+        --indicator \
+        --inside-color 282C34FF \
+        --ring-color 61AFEF \
+        --insidever-color 61AFEF \
+        --ringver-color 61AFEF \
+        --insidewrong-color E86681 \
+        --ringwrong-color E86671 \
+        --keyhl-color 98C379 \
+        --bshl-color E86671 \
+        -c 282C34FF \
+        --time-color 61AFEF \
+        --date-color 61AFEF
+fi
