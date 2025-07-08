b0590cae1ad1cd805600a03156c974be47604731
Jan <jan.peterhaensel@adconova.com>
2025-07-08 16:45:40 +0200

    Update to new version

--- nixos/home.nix
+++ nixos/home.nix
@@ -29,7 +29,6 @@
           "github.com" = {
             hostname = "github.com";
             user = "git";
-            identityFile = "~/.ssh/id_ed25519_github";
             identitiesOnly = true;
           };
         };
